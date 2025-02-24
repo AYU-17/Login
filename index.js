52e69a1358110c9cf959cb61a098425083427716
Aayush mittal <ayushmittal844@gmail.com>
2025-02-24 12:59:56 +0530

    try3

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv";
 
 
 const app = express();
 const port = 3000;
+env.config();
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "users",
-    password: "ayush@123",
-    port: 5432,
+user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
@@ -40,9 +42,7 @@ app.post("/register", async (req, res) => {
     const name = req.body.name;
 
     try {
-        const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
-            email,
-        ]);
+        const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [email]);
 
         if (checkResult.rows.length > 0) {
             // res.send("Email already exists. Try logging in.");
